60661aa5a7d71da391390509c6f6668ff37484ab
Martyn <martynrdavies@gmail.com>
2013-05-21 16:40:30 +0100

    added README

---
+++ README.md
@@ -0,0 +1,2 @@
+== sendgrid-tempodb-proxy ==
+A simple routing server that accepts input from SendGrid's Event API and passes the data through to the TempoDB time series database
--- proxy.js
+++ proxy.js
@@ -1,5 +1,5 @@
 var request = require('request'),
-		net = require('net'),
-		TempoDBClient = require('tempodb').TempoDBClient;
+var net = require('net'),
+var TempoDBClient = require('tempodb').TempoDBClient;
 
-var tempodb = new TempoDBClient('api-key', 'api-secret');
+var tempodb = new TempoDBClient(process.env.tempdb_api_key, process.env.tempdb_api_secret);
